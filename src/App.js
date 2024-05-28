2979f304845e1f26c8a46e0b966ba6fbffc75e8f
HEVIN-KUMAR-62 <hevinkumaramalraj@gmail.com>
2024-05-28 11:48:28 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+
+  return (
+    <div className='container'>
+      <button>button-one</button>
+      <button>button-two</button>
+      <button>button-three</button>
+      <button>button-four</button>
+    </div>
+  )
+}
+export default App ;
